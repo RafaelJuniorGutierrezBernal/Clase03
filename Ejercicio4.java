f700694950327851f2e67b1420a2d90c8d1b2ad6
Rafael Junior Gutierrez Bernal <rafas8921@gmail.com>
2024-10-30 11:25:42 -0500

    Ejercicio 4

---
+++ Ejercicio4.java
@@ -0,0 +1,12 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        
+        var DineroDeCarmen = 23d;
+        var DineroSobrante = 12.75;
+        
+        var TotalDeDinero = DineroDeCarmen - DineroSobrante;
+        System.out.println("Total de dinero: " + TotalDeDinero);
+        // en total se ha gastado: 10.25
+    }
+    
+}

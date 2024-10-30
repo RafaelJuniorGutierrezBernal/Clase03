b27b777cf1e1656994e29742a9ca5f7c1edfc6fe
Rafael Junior Gutierrez Bernal <rafas8921@gmail.com>
2024-10-30 11:01:58 -0500

    Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,16 @@
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        var operacion1 = 6 / 2 * (1 + 2);
+        /*  en mi caso el resultado que me dio es 9 y siento que es el acertado
+            teniendo en cuenta el orden de los operadores y como es la secuencia que toma la maquina de izquierda
+            a derecha
+        */
+        System.out.println(operacion1);
+        /*la otra calculadora pudo haber generado un resultado diferente debido
+         * al orden de las operaciones y al como esta progamada
+         */
+    }
+    
+}

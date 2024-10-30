e7d79881e89f53e59620ef651e69e2c4304ef1ff
Rafael Junior Gutierrez Bernal <rafas8921@gmail.com>
2024-10-30 11:20:39 -0500

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,13 @@
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        
+        var pecesRojos = 284;
+        var pecesAzules = 163;
+
+        var pecesTotal = pecesRojos + pecesAzules;
+        System.out.println(pecesTotal);
+        // el total correcto de peces en el acuario es: 447
+    }
+    
+}

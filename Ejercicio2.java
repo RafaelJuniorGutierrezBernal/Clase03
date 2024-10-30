113abe5d3031b96e9609076832d37a721a2bd933
Rafael Junior Gutierrez Bernal <rafas8921@gmail.com>
2024-10-30 11:17:11 -0500

    Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,13 @@
+public class Ejercicio2 {
+    
+    public static void main(String[] args) {
+    
+        double  porcionDe100Gramos = 5.95;
+
+        double  precioPorKilo = porcionDe100Gramos * 10;
+
+        System.out.println("El precio por kilo es: " + precioPorKilo);
+        // el precio correcto del kilo es: 59.5
+    }
+
+}
